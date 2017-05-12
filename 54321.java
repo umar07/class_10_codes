d26bccf38e9be91affbf4cbdcbf4e29a937c62df
sara-02 <sarahmasud02@gmail.com>
2017-05-12 21:04:45 +0530

    first commit

---
+++ 54321.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,b,p=1,j;
+		for(i=5;i>=1;i--)
+		{
+			for(b=1;b<=p;b++)
+			{
+				System.out.print(" ");
+				
+				
+			}
+			for( j=i;j>=1;j--)
+			{
+							System.out.print(j);
+							
+			}
+									System.out.println();
+
+						p++;
+			
+		}
+	}
+}
---
+++ pascal.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,b,p=5,j;
+		for(i=1;i<=5;i++)
+		{
+			for(b=1;b<=p;b++)
+			{
+				System.out.print(" ");
+				
+				
+			}
+			for( j=1;j<=i;j++)
+			{
+							System.out.print(j);
+			}
+			for(int k=j-2;k>=1;k--)
+			{
+							System.out.print(k);
+
+			}
+									System.out.println();
+
+						p--;
+			
+		}
+	}
+}
